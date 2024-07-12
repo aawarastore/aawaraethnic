982b46ff773a4cc4cb13d9b5ba16071e55279111
PratikBhopi <159427978+PratikBhopi@users.noreply.github.com>
2024-07-12 17:42:26 +0530

    Update Footer.jsx

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,9 +6,7 @@ const Footer = () => {
     <>
       <div className='w-screen py-10 px-10 bg-[#0c0c0cd7] flex flex-wrap flex-col items-center  justify-center content-center  text-white' >
       <div className='subtextLines text-yellow-600  px-2 py-2 text-[20px] '><Link to={'/home'}><span className='QuoteLines'>A</span>awara <span className='QuoteLines'>E</span>thnic</Link></div>
-      {/* <div className='subtextLines mb-8 w-1/2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, quasi! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, eius?</div> */}
-        <div className='flex gap-4  ss:text-[15px] text-[12px] '>
-        {/* <div className='flex gap-4'> */}
+        <div className='flex gap-4  ss:text-[15px] text-[10px] '>
           <Link to={'/'}><div className='hover:underline transition-all'>Contact Us</div></Link>
           <Link to={'/privacypolicy'}><div className='hover:underline transition-all'>Privacy Policy</div></Link>
           <Link to={'/shippingpolicy'}><div className='hover:underline transition-all'>Shipping Policy</div></Link>
@@ -16,12 +14,7 @@ const Footer = () => {
           <Link to={'/cancelationpolicy'}><div className='hover:underline transition-all'>Cancelation Policy</div></Link>
           <Link to={'/termsandcondition'}><div className='hover:underline transition-all'>Terms and Condition</div></Link>
          
-        {/* </div> */}
-
-        {/* <div className='hover:underline transition-all'>
-          Collections
-        </div> */}
-        {/* <div className='hover:underline transition-all'>Contact Us</div> */}
+    
 
       </div>
       </div>
